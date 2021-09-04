b4100b0695046708dbd35ac241d1934b3139078e
Lemoney <railkill@gmail.com>
2021-09-04 14:48:13 +0800

    Face player towards direction of movement

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -11,7 +11,9 @@ const DIRECTIONS = {
 }
 
 # Movement speed.
-export var speed = 15
+export var move_speed = 15
+# Rotation speed.
+export var rotation_speed = 15
 
 # Direction in which the gravity acts in the game world.
 var gravity_vector: Vector3 = \
@@ -24,24 +26,37 @@ var velocity = Vector3.ZERO
 
 # Player camera.
 onready var camera = $Camera
+# Player model.
+onready var model = $Model
 
 
 func _physics_process(delta):
+	# Simulate gravity.
 	velocity += gravity_vector * gravity_magnitude * delta
-	move()
+	
+	# Rotate the model if the player moved.
+	if(move()):
+		# Face model towards direction of movement.
+		model.rotation.y = lerp_angle(model.rotation.y, 
+				atan2(velocity.x, velocity.z), delta * rotation_speed)
+	
 	velocity = move_and_slide(velocity, Vector3.UP)
 
 
-# Move player according to directional input.
+# Move player according to input. Returns true if direction was pressed.
 func move():
 	# Preserve jump/fall velocity.
 	var vertical = velocity.y
+	var is_moving = false
 	
 	# Add velocity depending on directional input.
 	velocity = Vector3()
 	for vector in DIRECTIONS:
 		if Input.is_action_pressed(vector):
-			velocity += DIRECTIONS[vector] * speed
+			is_moving = true
+			velocity += DIRECTIONS[vector] * move_speed
 	
 	# Restore jump/fall velocity.
 	velocity.y = vertical
+	
+	return is_moving
