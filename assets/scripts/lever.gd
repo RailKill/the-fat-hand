c7ef7c02642c63079b599bab4f830d7610b6740c
Lemoney <railkill@gmail.com>
2021-09-06 02:25:50 +0800

    Setup main scene and floor button

--- assets/scripts/lever.gd
+++ assets/scripts/lever.gd
@@ -14,6 +14,23 @@ func _ready():
 func _physics_process(_delta):
 	if abs(knob.translation.x) > limit:
 		knob.mode = RigidBody.MODE_KINEMATIC
+		print(knob.translation.x)
 		knob.translation.x = clamp(knob.translation.x, -limit + 1, limit - 1)
 		knob.mode = RigidBody.MODE_RIGID
-	print(knob.translation.x)
+	
+	if not knob.sleeping:
+		var adjusted = limit - 1
+		var positivize = knob.translation.x + adjusted
+		var volume = positivize / (adjusted * 2) * 100 if positivize else 0
+		adjust_volume(1, volume)
+		
+
+
+func adjust_volume(bus_index: int, value: float):
+	AudioServer.set_bus_mute(bus_index, value <= 0)
+	AudioServer.set_bus_volume_db(bus_index, value / 2 - 50)
+	#print(AudioServer.get_bus_volume_db(1))
+
+
+func _on_Knob_sleeping_state_changed():
+	print("is sleeping: " + str(knob.sleeping))
