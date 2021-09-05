79062d83c553d85320c3fc69eb45e6cb87ddd909
Lemoney <railkill@gmail.com>
2021-09-06 06:35:02 +0800

    Set not to repeat cinematics unless returned to main menu

--- assets/scripts/global.gd
+++ assets/scripts/global.gd
@@ -5,10 +5,16 @@ extends Node
 var is_fullscreen = false
 # Number of pizzas to generate in main menu.
 var number_of_wins = 0
+# Cinematics played.
+var cinematic_played = {1: false, 2: false}
 
 onready var music_player = $AudioStreamPlayer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func play(number):
+	cinematic_played[number] = true
+
+
+# Reset cinematics when game is started from main menu.
+func reset_cinematics():
+	cinematic_played = {1: false, 2: false}
